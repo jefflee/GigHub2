be0d2ff06bf587490538237834be2462ba389271
Jeff Lee <hellow.jeff@gmail.com>
2018-08-15 00:07:46 +0800

    improve the look and feel

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -10,7 +10,8 @@ namespace GigHub
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/underscore-min.js"));
+                        "~/Scripts/underscore-min.js",
+                        "~/Scripts/moment.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
