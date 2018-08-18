33e9e72ec56d46d408acb0a5ee029fff09332663
Jeff Lee <hellow.jeff@gmail.com>
2018-08-18 09:35:49 +0800

    refactor app.js - extract to private functions and variables.

--- GigHub/Scripts/app/app.js
+++ GigHub/Scripts/app/app.js
@@ -1,35 +1,35 @@
 ﻿var GigsController = (function GigsConrollerIIFE() {
-    var init = function () {
-        $('.js-toggle-attendance').click(function (e) {
-            var button = $(e.target);
-            if (button.hasClass("btn-default")) {
-                // WebApi read data from body. It read the key which is empty string to get data.
-                $.post('/api/attendances', { GigId: button.attr("data-gig-id") })
-                    .done(function () {
-                        button
-                            .removeClass('btn-default')
-                            .addClass('btn-info')
-                            .text('Going');
-                    })
-                    .fail(function () {
-                        alert('Something failed!');
-                    });
-            } else {
-                $.ajax({
-                    url: "/api/attendances/" + button.attr("data-gig-id"),
-                    method: "DELETE"
-                }).done(function () {
-                    button.removeClass("btn-info")
-                        .addClass("btn-default")
-                        .text("Going?");
-                }).fail(function () {
-                    alert("Somthing failed");
-                });
-            }
+    var button;
 
-        });
+    var init = function () {
+        $('.js-toggle-attendance').click(toggleAttendances);
 
     };
+
+    function toggleAttendances(e) {
+        button = $(e.target);
+        if (button.hasClass("btn-default")) {
+            // WebApi read data from body. It read the key which is empty string to get data.
+            $.post('/api/attendances', { GigId: button.attr("data-gig-id") })
+                .done(doneAction)
+                .fail(failAction);
+        } else {
+            $.ajax({
+                url: "/api/attendances/" + button.attr("data-gig-id"),
+                method: "DELETE"
+            }).done(doneAction).fail(failAction);
+        }
+
+    }
+
+    function doneAction() {
+        var text = (button.text() == "Going") ? "Going?" : "Going";
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    }
+
+    function failAction() {
+        alert("Somthing failed");
+    }
 
     return {
         init: init
