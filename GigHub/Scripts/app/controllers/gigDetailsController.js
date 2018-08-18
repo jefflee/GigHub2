a913743115f69856895c20be55c67e075ce84a12
Jeff Lee <hellow.jeff@gmail.com>
2018-08-18 12:10:50 +0800

    refactoring - make functions for done and fail

--- GigHub/Scripts/app/controllers/gigDetailsController.js
+++ GigHub/Scripts/app/controllers/gigDetailsController.js
@@ -1,32 +1,35 @@
 ﻿var GigDetailController = (function iifeGigDetailController() {
 
+    var followButton;
+
     var init = function initFunc() {
         $('.js-toggle-follow').click(function (e) {
-            var button = $(e.target);
+            followButton = $(e.target);
 
-            if (button.hasClass("btn-default")) {
+            if (followButton.hasClass("btn-default")) {
 
                 // WebApi read data from body. It read the key which is empty string to get data.
-                $.post('/api/followings', { FolloweeId: button.attr("data-user-id") })
-                    .done(function () {
-                        button.addClass("btn-info").removeClass("btn-default").text('Following');
-                    })
-                    .fail(function () {
-                        alert('Something failed!');
-                    });
+                $.post('/api/followings', { FolloweeId: followButton.attr("data-user-id") })
+                    .done(doneAction)
+                    .fail(failAction);
             } else {
                 $.ajax({
-                    url: "/api/followings/" + button.attr("data-user-id"),
+                    url: "/api/followings/" + followButton.attr("data-user-id"),
                     method: "DELETE"
-                }).done(function () {
-                    button.addClass("btn-default").removeClass("btn-info").text('Follow');
-                })
-                    .fail(function () {
-                        alert('Something failed!');
-                    });
+                }).done(doneAction)
+                    .fail(failAction);
             }
         });
     }
+
+    function doneAction() {
+        var text = (followButton.text() == "Follow") ? "Following" : "Follow";
+        followButton.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+
+    function failAction() {
+        alert("Something failed");
+    };
 
     return {
         init: init
