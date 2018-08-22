d99b99b5805550ce30579fa632e12222dab61cbc
Jeff Lee <hellow.jeff@gmail.com>
2018-08-22 22:38:24 +0800

    First test

--- GigHub.Tests/Controllers/Api/GigsControllerTests.cs
+++ GigHub.Tests/Controllers/Api/GigsControllerTests.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Security.Claims;
 using System.Security.Principal;
+using System.Web.Http.Results;
+using FluentAssertions;
 using GigHub.Controllers.Api;
 using GigHub.Core;
+using GigHub.Core.Repositories;
 using GigHub.Tests.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -16,15 +19,20 @@ namespace GigHub.Tests.Controllers.Api
 
         public GigsControllerTests()
         {
-
+            var mockRepository = new Mock<IGigRepository>();
 
             var mockUoW = new Mock<IUnitOfWork>();
+            mockUoW.SetupGet(u => u.Gigs).Returns(mockRepository.Object);
+
             _controller = new GigsController(mockUoW.Object);
             _controller.MockCurrentUser("1", "user1@domain.com");
         }
         [TestMethod]
-        public void TestMethod1()
+        public void Cancel_NoGigWithGivenIdExists_ShouldReturnNotFound()
         {
+            var result = _controller.Cancel(1);
+
+            result.Should().BeOfType<NotFoundResult>();
         }
     }
 }
