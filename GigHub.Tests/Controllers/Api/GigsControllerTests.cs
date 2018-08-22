8eb0858f34133b21f98f2741c21d805e647f6aba
Jeff Lee <hellow.jeff@gmail.com>
2018-08-22 22:08:25 +0800

    Mock current user

---
+++ GigHub.Tests/Controllers/Api/GigsControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Claims;
+using System.Security.Principal;
+using GigHub.Controllers.Api;
+using GigHub.Core;
+using GigHub.Tests.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GigHub.Tests.Controllers.Api
+{
+    [TestClass]
+    public class GigsControllerTests
+    {
+        private GigsController _controller;
+
+        public GigsControllerTests()
+        {
+
+
+            var mockUoW = new Mock<IUnitOfWork>();
+            _controller = new GigsController(mockUoW.Object);
+            _controller.MockCurrentUser("1", "user1@domain.com");
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ GigHub.Tests/Extensions/ApiControllerExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GigHub.Tests.Extensions
+{
+    public static class ApiControllerExtensions
+    {
+        public static void MockCurrentUser(this ApiController controller, string userId, string userName)
+        {
+            var identity = new GenericIdentity(userName);
+
+            identity.AddClaim(
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", userName));
+
+            identity.AddClaim(
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", userId));
+
+            var principal = new GenericPrincipal(identity, null);
+
+            controller.User = principal;
+        }
+    }
+}
