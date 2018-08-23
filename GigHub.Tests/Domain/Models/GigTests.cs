9bebedd13710d0f96c3ada77edb26fe0fe9f5f9f
Jeff Lee <hellow.jeff@gmail.com>
2018-08-23 23:56:06 +0800

    Add domain model testing.

---
+++ GigHub.Tests/Domain/Models/GigTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GigHub.Core.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GigHub.Tests.Domain.Models
+{
+    [TestClass]
+    public class GigTests
+    {
+        // A different approach to write these tests is using BDD. 
+        // 
+        // GivenAnArtistHasAGig
+        //      WhenHeCancelsTheGig
+        //          IsCanceledShouldBeSetToTrue
+        //          EachAttendeeShouldHaveANotification
+        //
+        // 
+        [TestMethod]
+        public void Cancel_WhenCalled_ShouldSetIsCanceledToTrue()
+        {
+            var gig = new Gig();
+
+            gig.Cancel();
+
+            gig.IsCanceled.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Cancel_WhenCalled_EachAttendeeShouldHaveANotification()
+        {
+            var gig = new Gig();
+            gig.Attendances.Add(new Attendance { Attendee = new ApplicationUser { Id = "1" } });
+
+            gig.Cancel();
+
+            //TODO: This could be pushed into the Gig class (eg gig.GetAttendees())
+            var attendees = gig.Attendances.Select(a => a.Attendee).ToList();
+            attendees[0].UserNotifications.Count.Should().Be(1);
+        }
+    }
+}
