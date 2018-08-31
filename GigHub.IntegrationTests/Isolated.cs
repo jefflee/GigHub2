75b1b071f8b7031898bbee6a7790ed3be55dcf4c
Jeff Lee <hellow.jeff@gmail.com>
2018-09-01 01:10:50 +0800

    Isolating test data - add an attribute, it will create transactionscope before test case and roll back data after test case.

---
+++ GigHub.IntegrationTests/Isolated.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace GigHub.IntegrationTests
+{
+    public class Isolated : Attribute, ITestAction
+    {
+        private TransactionScope _transactionScope;
+        public void BeforeTest(ITest test)
+        {
+            _transactionScope = new TransactionScope();
+        }
+
+        public void AfterTest(ITest test)
+        {
+            _transactionScope.Dispose();
+        }
+
+        public ActionTargets Targets
+        {
+            get { return ActionTargets.Test; }
+        }
+    }
+}
