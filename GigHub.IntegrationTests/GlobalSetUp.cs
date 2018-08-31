57916cda89fca659f8c84f4da485a2ea79807198
Jeff Lee <hellow.jeff@gmail.com>
2018-09-01 00:59:42 +0800

    Seeding data - create user data in SetUp

--- GigHub.IntegrationTests/GlobalSetUp.cs
+++ GigHub.IntegrationTests/GlobalSetUp.cs
@@ -4,6 +4,8 @@ using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GigHub.Core.Models;
+using GigHub.Persistence;
 using NUnit.Framework;
 
 namespace GigHub.IntegrationTests
@@ -14,10 +16,31 @@ namespace GigHub.IntegrationTests
         [OneTimeSetUp]
         public void SetUp()
         {
+            MigrateDbToLatestVersion();
+
+            Seed();
+        }
+
+        public void MigrateDbToLatestVersion()
+        {
             var configuration = new GigHub.Migrations.Configuration();
             var migrator = new DbMigrator(configuration);
             migrator.Update();
         }
-        
+
+        public void Seed()
+        {
+            var context = new ApplicationDbContext();
+
+            if (context.Users.Any())
+            {
+                return;
+            }
+
+            context.Users.Add(new ApplicationUser { UserName = "user1", Name = "user1", Email = "-", PasswordHash = "-" });
+            context.Users.Add(new ApplicationUser { UserName = "user2", Name = "user2", Email = "-", PasswordHash = "-" });
+            context.SaveChanges();
+        }
+
     }
 }
